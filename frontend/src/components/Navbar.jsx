a52c00b49b791d9ad36740bbe7e986aeaf13764c
Pratham 98max <shelken435@gmail.com>
2025-12-24 01:48:32 +0530

    Add logout functionality to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/firebaseConfig";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    navigate("/");
+  };
+
   return (
     <nav style={{ padding: "15px", borderBottom: "1px solid #ccc" }}>
       <h3 style={{ display: "inline", marginRight: "20px" }}>BINNECT</h3>
@@ -9,13 +18,7 @@ function Navbar() {
         Home
       </Link>
 
-      <Link to="/search" style={{ marginRight: "15px" }}>
-        Search
-      </Link>
-
-      <Link to="/register-business">
-        Register Business
-      </Link>
+      <button onClick={handleLogout}>Logout</button>
     </nav>
   );
 }
